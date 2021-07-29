2446783b25a0f7adf8c2ca8d0aaf6919ea6d36da
mj1111999 <99mohitjain@gmail.com>
2021-07-29 21:17:50 +0530

    Updates

---
+++ precache-manifest.08526655ab1be23cc67d315f2fc53551.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "940dd8f0c0a51f4710b9f9adc29a880a",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "7771ebc8e7368276800d",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "de32571a899bea417cb8",
+    "url": "/box-office/static/js/2.6a1cb71c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.6a1cb71c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7771ebc8e7368276800d",
+    "url": "/box-office/static/js/main.9c29626a.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
