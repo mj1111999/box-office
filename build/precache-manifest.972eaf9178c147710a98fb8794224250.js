d66d0dda931035c2092fc126e0dbe525ae7ad1ed
mj1111999 <99mohitjain@gmail.com>
2021-07-29 12:27:01 +0530

    commited changes, styled websites and ready for deployment

---
+++ build/precache-manifest.972eaf9178c147710a98fb8794224250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c48298efe5e379ab5bf5250de167647b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be6b4108c948dc3746b7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "51c87f264cd2c50ebefe",
+    "url": "/static/js/2.d892589f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d892589f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be6b4108c948dc3746b7",
+    "url": "/static/js/main.8266b690.chunk.js"
+  },
+  {
+    "revision": "8b0fe88b199229d7b813",
+    "url": "/static/js/runtime-main.8d00ed74.js"
+  }
+]);
